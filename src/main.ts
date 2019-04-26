289cb4162b23d1ed990e46f0b191d98933fbc1d7
simon-an <nomisimon@posteo.de>
2019-04-26 10:52:23 +0200

    for loop solution

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,30 @@
 export function dictonaryReplacer(dictionary: { [key: string]: string }, text: string) {
-    return ''
+  if (text.length <= 0) {
+    return '';
+  }
+
+  let indices: number[] = [];
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === '$') {
+      indices.push(i);
+    }
+  }
+
+  if (indices.length % 2 === 1) {
+    throw new Error('invalid amout of $');
+  }
+
+  let newString: string = '';
+  if (indices[0] > 0) {
+    newString += text.substring(0, indices[0]);
+  }
+  for (let index = 0; index < indices.length; index++) {
+    const variableName = text.substring(indices[index] + 1, indices[index + 1]);
+    if (index % 2 == 0) {
+      newString += dictionary[variableName];
+    } else {
+      newString += variableName;
+    }
+  }
+  return newString;
 }
--- test/test.ts
+++ test/test.ts
@@ -14,4 +14,36 @@ describe('tests ', () => {
     const result = dictonaryReplacer(dictionary, '\$temp\$');
     expect(result).to.equal('temporary');
   });
+  it('two variables', () => {
+    const dictionary = {
+        temp: 'temporary',
+        name: 'John Doe'
+    };
+    const result = dictonaryReplacer(dictionary, '\$temp\$ here comes the name \$name\$');
+    expect(result).to.equal('temporary here comes the name John Doe');
+  });
+  it('two variables without text between', () => {
+    const dictionary = {
+        temp: 'temporary',
+        name: 'John Doe'
+    };
+    const result = dictonaryReplacer(dictionary, '\$temp\$\$name\$');
+    expect(result).to.equal('temporaryJohn Doe');
+  });
+  it('two variables with text before first variable', () => {
+    const dictionary = {
+        temp: 'temporary',
+        name: 'John Doe'
+    };
+    const result = dictonaryReplacer(dictionary, 'text before \$temp\$ here comes the name \$name\$');
+    expect(result).to.equal('text before temporary here comes the name John Doe');
+  });
+  it('two variables with text after last variable', () => {
+    const dictionary = {
+        temp: 'temporary',
+        name: 'John Doe'
+    };
+    const result = dictonaryReplacer(dictionary, 'text before \$temp\$ here comes the name \$name\$ text after');
+    expect(result).to.equal('text before temporary here comes the name John Doe text after');
+  });
 });
